e885b1c07144f6b59d72b70036f79f1ccc9c27ca
lokeshe-09 <lokeshe0912@gmail.com>
2024-10-02 22:48:47 +0530

    Update app.py

--- Ai-Aca/app.py
+++ Ai-Aca/app.py
@@ -155,7 +155,7 @@ def set_page_config():
     footer {visibility: hidden;}
     header {visibility: hidden;}
     .stApp {
-        max-width: 1200px;
+        max-width: 100vu;
         margin: 0 auto;
         font-family: Arial, sans-serif;
     }
